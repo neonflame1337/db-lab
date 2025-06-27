6710059835f3649ab44ba9168acecef2c0b1e4e2
Artem Krysan <artem.krysan@solidgate.com>
2025-06-27 11:51:23 +0300

    s

--- src/main/kotlin/com/kpi/lab/persistence/postgres/repository/TransactionRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.kpi.lab.persistence.postgres.repository
-
-import com.kpi.lab.persistence.postgres.entity.TransactionEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import java.util.UUID
-
-@Repository
-interface TransactionRepository: JpaRepository<TransactionEntity, UUID>
